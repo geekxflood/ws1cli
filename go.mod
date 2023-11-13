b88523491ebd6394fcc520994159af6c25770cff
Christophe <christopherime@me.com>
2023-11-13 06:50:05 +0000

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module christopherime/ws1-cli
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// START OMIT
+	// Create a new instance of the application
+	app := NewApp()
+
+	// Start the application
+	app.Start()
+	// END OMIT
+}
